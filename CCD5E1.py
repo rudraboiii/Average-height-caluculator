807a1fd2e85d0734af76ecd1789eac5a0be19972
RUDRABOIII <105966888+rudraboiii@users.noreply.github.com>
2023-07-28 14:22:51 +0530

    Add files via upload

--- CCD5E1.py
+++ CCD5E1.py
@@ -1,13 +1,8 @@
 
-
-# 🚨 Don't change the code below 👇
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
 
-
-#Write your code below this row 
 
 num_of_students = 0 
 for student in student_heights:
