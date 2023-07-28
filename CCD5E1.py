4143521a9261a136f807b955e139effa28e35e24
RUDRABOIII <105966888+rudraboiii@users.noreply.github.com>
2023-07-28 14:19:05 +0530

    Add files via upload

---
+++ CCD5E1.py
@@ -0,0 +1,23 @@
+
+
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 
+
+num_of_students = 0 
+for student in student_heights:
+  num_of_students += 1
+# print(num_of_students)
+
+total_height = 0 
+for height in student_heights:
+  total_height += height 
+# print(total_height)
+
+height_average = round(total_height / num_of_students)
+print(f"Average Student height is :{height_average}")
